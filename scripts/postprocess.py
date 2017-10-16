d3d09ea64e0607283d82b34ad3bb7dcbcbe4774e
gaarangoa <gaarangoa@gmail.com>
2017-10-16 11:57:39 -0400

    a

--- scripts/postprocess.py
+++ scripts/postprocess.py
@@ -9,7 +9,7 @@ M = {}
 for i in open(metadata):
     i = i.split()
     try:
-        M[i[1]].append(i[2]+".uc.genes")
+        M[i[1]].append(pathd+"/"+i[2]+".uc.genes")
     except:
         M[i[1]] = [pathd+"/"+i[2]+".uc.genes"]
 
